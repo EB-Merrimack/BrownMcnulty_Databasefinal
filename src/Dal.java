1ec4071ec4d536377b72714b4d60c406dc9482b7
EB-Merrimack <144274749+EB-Merrimack@users.noreply.github.com>
2024-04-29 14:26:03 -0400

    cleaning code

--- src/Dal.java
+++ src/Dal.java
@@ -20,17 +20,6 @@ public class Dal {
     public Dal() {
     }
 
-    private static Connection getMySQLConnection(String databaseName, String user, String password) {
-        try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, user, password);
-        } catch (SQLException var5) {
-            System.out.println("Failed to connect to the database" + var5.getMessage());
-            return null;
-        }
-    }
-
-    private static final String SELECT_ITEM_QUERY = "SELECT * FROM restaurants WHERE RestaurantName= ?";
-
     public List<String> searchInventory(String dbName, String parksearch, String username, String password) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'searchInventory'");
